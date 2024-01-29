72ee8e9dc8acfc3ed48ddae2785b17a0a8c16df7
c0deFinder <alejandromiranda450@gmail.com>
2024-01-28 21:18:13 -0400

    changes

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -2,45 +2,49 @@ import { useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { MyContext } from "../Context/MyContext";
 import { Button, Card } from "react-bootstrap";
-import './Pizza.css'
+import './Pizza.css';
 
- const Pizza = () => {
+const Pizza = () => {
   const { pizzas } = useContext(MyContext);
   const { id } = useParams();
   const navigate = useNavigate();
-  function addItem(pizza) {
-    const Cart_Shop = JSON.parse(localStorage.getItem("Cart_Shop")) || [];
-    Cart_Shop.push(pizza);
-    localStorage.setItem("Cart_Shop", JSON.stringify(Cart_Shop));
+
+  const addItem = (pizza) => {
+    const cartShop = JSON.parse(localStorage.getItem("Cart_Shop")) || [];
+    cartShop.push(pizza);
+    localStorage.setItem("Cart_Shop", JSON.stringify(cartShop));
     navigate("/carrito");
-
-  }
-
-  const SeachPizza = (id) => {
-    return pizzas.find((pizza) => pizza.id === id);
   };
 
-  const Pizza_1 = SeachPizza(id);
+  const searchPizza = (id) => {
+    const pizza = pizzas.find((p) => p.id === id);
+    if (!pizza) {
+      console.error(`Pizza with id ${id} not found.`);
+    }
+    return pizza;
+  };
 
-   const newLocal = "Cart_container";
+  const pizzaItem = searchPizza(id);
+
   return (
     <div>
-      <Card className={newLocal}>
-        <Card.Img className="CardImg_1" src={Pizza_1.img} />
+      <Card className="Cart_container">
+        <Card.Img className="CardImg_1" src={pizzaItem ? pizzaItem.img : ''} />
         <Card.Body>
-          <Card.Title>{Pizza_1.name}</Card.Title>
-          <Card.Text>{Pizza_1.desc}</Card.Text>
-          {Pizza_1.ingredients.map((ingredient) => (
+          <Card.Title>{pizzaItem ? pizzaItem.name : 'Pizza Not Found'}</Card.Title>
+          <Card.Text>{pizzaItem ? pizzaItem.desc : 'Description Not Available'}</Card.Text>
+          {pizzaItem && pizzaItem.ingredients.map((ingredient) => (
             <Card.Text key={ingredient}>{ingredient}</Card.Text>
           ))}
-          <Card.Text>Precio: ${Pizza_1.price}</Card.Text>
+          <Card.Text>{pizzaItem ? `Precio: $${pizzaItem.price}` : 'Price Not Available'}</Card.Text>
         </Card.Body>
         <div>
-          <Button  onClick={() => addItem(Pizza_1)}>Añadir</Button>
+          <Button onClick={() => addItem(pizzaItem)}>Añadir</Button>
           <Button className="Button_Cart" onClick={() => navigate("/home")}>Volver</Button>
         </div>
       </Card>
     </div>
   );
 };
-export default Pizza
+
+export default Pizza;
